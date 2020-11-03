9a8797bfac2364b79a44a35b02403ef1a6fedcca
José García Borge <adrian23gb@hotmail.com>
2020-11-03 09:36:15 -0600

    Se añade crear un XML

--- SistemaFacturas.BL/IRepositorioFactura.cs
+++ SistemaFacturas.BL/IRepositorioFactura.cs
@@ -16,12 +16,12 @@ namespace SistemaFacturas.BL
         public List<Producto> bus_atr(string dato_bus, int cantidad);
         public int Disponible(int codProducto, int cantidad);
         public Producto producto(string CodProducto);
+        void AgregarCliente(Persona persona);
+        public List<Persona> ListaClientes();
+
         void XML(Facturar facturar);
         void reporte(Factura nuevaFactura);
         List<Reporte> reportes();
         List<Factura> facturas();
-        void AgregarCliente(Persona persona);
-        public List<Persona> ListaClientes();
-
     }
 }
